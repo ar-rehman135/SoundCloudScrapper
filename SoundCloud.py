63c957be3041e0fd4040ac750b8af7081a7c0f7c
ubaidmanzoor987 <ubaidmanzoor987@gmail.com>
2021-04-02 21:11:50 +0500

    updates

--- SoundCloud.py
+++ SoundCloud.py
@@ -125,7 +125,7 @@ def get_chromedriver():
 
 driver, PROXY, excludes = get_chromedriver()
 
-initialurl = 'https://api-v2.soundcloud.com/search/users?q=rapper&sc_a_id=577a677d42d8462a4c35f86450c82c61d176ba95&variant_ids=2077&facet=place&user_id=825873-12912-268615-711068&client_id=OBhq4Cf6jGxc2cbaKvzxJX8QQLKXEryc&limit=100&offset=<offset>&linked_partitioning=1&app_version=1616065176&app_locale=en'
+initialurl = 'https://api-v2.soundcloud.com/search/users?q=rapper&sc_a_id=577a677d42d8462a4c35f86450c82c61d176ba95&variant_ids=2077&facet=place&user_id=825873-12912-268615-711068&client_id=OBhq4Cf6jGxc2cbaKvzxJX8QQLKXEryc&limit=10&offset=<offset>&linked_partitioning=1&app_version=1616065176&app_locale=en'
 
 def sleep_and_find(find_sel):
     for i in range(20):
@@ -165,7 +165,7 @@ def main():
 
         print("Data Scrapping...")
 
-        entry_count = int(entries) / 100
+        entry_count = int(entries) / 10
 
         if entry_count < 1:
             entry_count = 1
@@ -187,7 +187,7 @@ def main():
             portalocker.lock(file, portalocker.LOCK_EX)
 
             for n in range(round(entry_count)):
-                url = initialurl.replace('<offset>', str((n * 100)+int(entries_start)))
+                url = initialurl.replace('<offset>', str((n * 10)+int(entries_start)))
                 try:
                     data = requests.get(url, proxies=proxies)
                 except:
@@ -231,22 +231,54 @@ def main():
 
                         IG_username, IG_URL = None, None
                         if re.search(r'\binstagram\b', bio, re.IGNORECASE):
-                            web_profiles = driver.find_element_by_xpath('//*[@id="content"]/div/div[4]/div[2]/div/article[1]/div[2]/ul')
-                            web_profiles_list = web_profiles.find_elements_by_tag_name('li')
-                            for i in range(len(web_profiles_list)):
-                                href = driver.find_element_by_xpath('//*[@id="content"]/div/div[4]/div[2]/div/article[1]/div[2]/ul/li[' + str((i+1)) + ']/div/a').get_attribute('href')
-                                if 'instagram' in href:
-                                    IG_username = re.search(r'%2F(.*?)%2F', href)
-                                    print(IG_username)
-                                    IG_URL = "https://www.instagram.com/" + IG_username.group(1)
+                            web_profiles = sleep_and_find('//*[@id="content"]/div/div[4]/div[2]/div/article[1]/div[2]/ul')
+                            if web_profiles:
+                                web_profiles_list = web_profiles.find_elements_by_tag_name('li')
+                                for i in range(len(web_profiles_list)):
+                                    href = sleep_and_find('//*[@id="content"]/div/div[4]/div[2]/div/article[1]/div[2]/ul/li[' + str((i+1)) + ']/div/a').get_attribute('href')
+                                    if 'instagram' in href and href is not None:
+                                        # IG_username = re.search(r'%2F(.*?)%2F', href)
+                                        IG_username = href.split(".com%2F")[1].split('&')[0]
+                                        if not IG_username.find("%2F") == -1:
+                                            IG_username = IG_username.split("%2F")[0]
+                                        IG_URL = "https://www.instagram.com/" + IG_username
+                        if re.search(r'\bIG: @\b', bio, re.IGNORECASE):
+                            web_profiles = sleep_and_find(
+                                '//*[@id="content"]/div/div[4]/div[2]/div/article[1]/div[1]/div/div/div/div/p[5]/a[2]')
+                            if web_profiles:
+                                IG_username = web_profiles.text
+                                IG_URL = "https://www.instagram.com/" + IG_username
 
                         if email is not None or IG_username is not None:
                             username = dataObj['collection'][i]['username']
                             permalink_url = dataObj['collection'][i]['permalink_url']
                             full_name = dataObj['collection'][i]['full_name']
-                            location = str(dataObj['collection'][i]['city']) + " " + str(dataObj['collection'][i]['country_code'])
+                            country = str(dataObj['collection'][i]['country_code'])
+                            if email:
+                                try:
+                                    email = email.group(0)
+                                except Exception as ex:
+                                    email = email.group(1)
+                                    print(ex)
+                            cn = ''
+                            if country:
+                                cn = country if country else ''
+                            if full_name == 'GEDZ':
+                                print(full_name)
+                            location = str(dataObj['collection'][i]['city'])
+                            location_sl = ''
+                            if not location.find("/") == -1:
+                                location_sl = sleep_and_find('//*[@id="content"]/div/div[2]/div/div[1]/div/div[2]/h4[2]')
+                                location_sl = location_sl.text
+                                try:
+                                    location_sl = str(location_sl).split(", ")[1]
+                                except:
+                                    location_sl = ''
+                            if location_sl == '':
+                                location = location_sl +  " " + cn
+                            else:
+                                location = location + " " + cn
                             name_title = sleep_and_find('//*[@id="content"]/div/div[4]/div[1]/div/div[2]/div/ul/li[1]/div/div/div[2]/div[1]/div/div/div[2]/a/span')
-
                             try:
                                 name_title = name_title.text
                             except:
@@ -314,10 +346,6 @@ def main():
                                 except:
                                     pass
 
-
-
-
-
                                 try:
                                     song_title = name_title.split('-')[1] + name_title.split('-')[2]
                                 except:
@@ -356,7 +384,7 @@ def main():
                                 except:
                                     pass
 
-
+                                song_title = name_title
                                 song_title_full = name_title
                                 try:
                                     song_title = name_title.split('(')[0]
@@ -387,23 +415,30 @@ def main():
                                 full_name2 = rapper_name.strip()
 
                             song_title = song_title.strip()
-                            if song_title.isascii():
-                                song_title = song_title
-                            else:
-                                song_title = ''
+                            # if song_title.isascii():
+                            #     song_title = song_title
+                            # else:
+                            #     song_title = ''
+                            song_title = song_title.encode("ascii", "ignore")
+                            song_title = song_title.decode()
+                            full_name2 = full_name2.encode("ascii", "ignore")
+                            full_name2 = full_name2.decode()
 
-                            print(email.group(0))
-                            print(IG_username.group(1))
+                            if email:
+                                if not email.find(":") == -1:
+                                    email = email.split(":")[1]
+                                if not email.find("/") == -1:
+                                    email = email.split("//")[1]
 
-                            rapper_list = [username, rapper_name.strip(), full_name2, location, permalink_url,
-                                           email.group(0), song_title, song_title_full, IG_URL, IG_username.group(1)]
+                            rapper_list = [username, rapper_name.strip(), full_name2, location, country, permalink_url,
+                                           email, song_title, song_title_full, IG_URL, IG_username]
                             rapper_data.append(rapper_list)
 
                 if len(rapper_data) > 0:
                     count += 1
                     portalocker.unlock(file)
                     rapper_data_df = pd.DataFrame(rapper_data)
-                    rapper_data_df.columns = ['Username', 'FullName', 'FullName2', 'Location', 'RapperURL',
+                    rapper_data_df.columns = ['Username', 'FullName', 'FullName2', 'Location', 'Country', 'RapperURL',
                                               'Email', 'SongTitle', 'SongTitleFull', 'InstagramURL', 'InstagramUserName']
                     if count == 1:
                         rapper_data_df.to_csv(filename, mode='a', index=False, header=True)
--- test2.py
+++ test2.py
@@ -1,9 +1,14 @@
 import re
 s = 'https://gate.sc/?url=http%3A%2F%2Finstagram.com%2Ftroubledte6&token=9bc1d3-1-1616572838086'
-s1 = 'Part 1. Part 2. Part 3 then more text'
+s1 = 'https://gate.sc/?url=https%3A%2F%2Fwww.instagram.com%2Fwarhol.ss%2F%3Fhl%3Den&token=dabdb8-1-1617369807034'
+# s1 = 'Part 1. Part 2. Part 3 then more text'
 
-r = re.search(r'com%2F(.*?)&', s)
-r1 = re.search(r'Part 1(.*?)Part 3', s1).group(1)
+r = s1.split(".com%2F")[1].split('&')[0]
+if not r.find("%2F") == -1:
+    r = r.split("%2F")[0]
+# if r.contains('%2F'):
+# r = re.search(r'com(.*?)&', s).group(0).
+# r1 = re.search(r'Part 1(.*?)Part 3', s1).group(1)
 
 print(r)
-print(r1)
+# print(r1)
